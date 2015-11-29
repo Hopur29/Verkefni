c89086e6f55a15708bbd573ef96542eb3e37ab2d
Ævar þór Gunnlaugsson <aevartg@vars-MBP.lan>
2015-11-29 17:15:49 +0000

    prufa

--- FamousComSci/manager.cpp
+++ FamousComSci/manager.cpp
@@ -1,5 +1,6 @@
 #include "manager.h"
 #include <fstream>
+#include <iostream>
 
 using namespace std;
 
@@ -11,10 +12,20 @@ Manager::Manager()
 void Manager::init()
 {
     ifstream read;
-    read.open("data.txt");
-    Scientist next;
-    while (read >> next)
-        vec.push_back(next);
+    read.open("data.txt", fstream::in | fstream::out | fstream::app);
+
+    if (read.is_open())
+      {
+        cout << "hello";
+        Scientist s;
+
+               add(s);
+
+      }
+      else
+      {
+        cout << "Error opening file";
+      }
 }
 
 void Manager::add(Scientist s)
@@ -24,7 +35,7 @@ void Manager::add(Scientist s)
 
 void Manager::print()
 {
-    for(int i = 0; i < vec.size(); i++)
+    for(unsigned int i = 0; i < vec.size(); i++)
     {
         cout << vec[i];
     }
